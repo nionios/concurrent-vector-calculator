871d58362c2a116e53e6aef537a843c257f6770b
nnis <dennis.nik@protonmail.com>
2022-05-01 19:15:10 +0300

    Fix sanitary_int function

--- src/human/rpcvec_client.c
+++ src/human/rpcvec_client.c
@@ -7,53 +7,62 @@
 #include "prompts.h"
 #include "../rpc/rpcvec.h"
 
-//FIXME: make the sanitary_* functions work, they spit out NULL all the time
-void sanitary_int(int * intp) {
+void
+sanitary_int(int * intp) {
     char line[256];
     int scanned;
     if (fgets(line, sizeof(line), stdin)) {
         if (1 == sscanf(line, "%d", &scanned))
-            intp = &scanned;
+            *intp = scanned;
         else intp = NULL;
     }
 }
 
-void sanitary_double(double * dblp) {
+//FIXME: make the sanitary_double function work, it spits out -0.000 all the time
+void
+sanitary_double(double * dblp) {
     char line[256];
     double scanned;
     if (fgets(line, sizeof(line), stdin)) {
         if (1 == sscanf(line, "%lf", &scanned))
-            dblp = &scanned;
+            *dblp = scanned;
         else dblp = NULL;
     }
 }
 
-void client_side(CLIENT *clnt){
+void
+client_side(CLIENT *clnt){
     unsigned int choice;
-    int * input_size;
+    int input_size;
+    int * sizep = &input_size;
     int flag=1;
     while(flag){
         fprintf(stdout,"\nPlease provide number of elements for vector: ");
-        sanitary_int(input_size);
-        if (input_size == NULL) {
-            fprintf(stdout,"\nError, invalid input type:"\
+        sanitary_int(sizep);
+        // If input is not valid, sanitary_int returns a null pointer
+        if (sizep == NULL) {
+            fprintf(stderr,"\nError, invalid input type:"\
                     "Please input an integer value from 1 to 255");
-        } else if (*input_size >= 1 && *input_size <= 255) {
-            fprintf(stdout,"\nError, invalid input integer:"\
+        } else if (input_size < 1 || input_size > 255) {
+            fprintf(stderr,"\nError, invalid input integer:"\
                     "Please input an integer value from 1 to 255");
-        }
+        // If no problems occur, continue normally.
+        } else break;
     }
     fprintf(stdout,"*** Concurrent Vector Calculator ***");
     vec vector;
-    vector.vec_val = malloc(sizeof(double) * (*input_size));
-    vector.vec_len = *input_size;
+    vector.vec_val = malloc(sizeof(double) * input_size);
+    vector.vec_len = input_size;
     for (int i=0; i<vector.vec_len; i++) {
+        //Set a pointer to current array position to feed into sanitary_double
+        double * valp = &vector.vec_val[i];
         fprintf(stdout,"\nPlease provide element number %d of vector: ",i);
-        sanitary_double(&vector.vec_val[i]);
-        if (&vector.vec_val[i] == NULL) {
-            fprintf(stdout,"Error, invalid input type:"\
+        sanitary_double(valp);
+        // If input is not valid, sanitary_double returns a null pointer
+        if (valp == NULL) {
+            fprintf(stderr,"Error, invalid input type:"\
                     "Please input a valid double value");
-            //Go back a step if the input is wrong
+            //Repeat current step if the input is wrong
             i--;
         }
         printf("\nvector.vec_val[%d] == %lf",i,vector.vec_val[i]);
@@ -87,10 +96,11 @@ void client_side(CLIENT *clnt){
     }
 }
 
-int main(int argc,char **argv) {
+int
+main(int argc,char **argv) {
     char * host;
     if (argc < 2) {
-        printf ("rpcvec_client - usage: %s server_host\n", argv[0]);
+        fprintf(stderr,"rpcvec_client - usage: %s server_host\n", argv[0]);
         exit(1);
     }
     host = argv[1];
